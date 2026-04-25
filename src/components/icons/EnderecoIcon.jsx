eeb7824204dedb04c338aba756cd138d74334768
ponsverus <ponsverus@gmail.com>
2026-04-25 15:26:21 -0300

    Update EnderecoIcon.jsx

--- src/components/icons/EnderecoIcon.jsx
+++ src/components/icons/EnderecoIcon.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 
-  export default function EnderecoIcon({ className = '', title, style = {}, ...props }) {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className={className}
-        style={{ display: 'inline-block', ...style }}
-        aria-hidden={title ? undefined : 'true'}
-        role={title ? 'img' : 'presentation'}
-        {...props}
-      >
-        {title && <title>{title}</title>}
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M12 2C8.686 2 6 4.686 6 8c0 4.5 6 13 6 13s6-8.5 6-13c0-3.314-2.686-6-6-6Z"
-        />
-        <circle cx="12" cy="8" r="2.2" fill="white" opacity="0.85" />
-      </svg>
-    );
-  }
+export default function LocationArrowIcon({
+  className = '',
+  title,
+  style = {},
+  size = 24,
+  ...props
+}) {
+  return (
+    <svg
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      width={size}
+      height={size}
+      className={className}
+      style={{
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        ...style,
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      role={title ? 'img' : 'presentation'}
+      aria-hidden={!title}
+      {...props}
+    >
+      {title && <title>{title}</title>}
+      <path
+        d="M 816 208 L 109 497 L 466 544 L 513 901 Z"
+      />
+    </svg>
+  );
+}
